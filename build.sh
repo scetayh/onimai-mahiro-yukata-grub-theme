748fd582c169e9372c2719f4ccbb55a5fadecf35
scetayh <weychon_lyon@outlook.com>
2026-08-27 12:04:05 +0800

    feat: add multiple language support and use arrays to express options

--- build.sh
+++ build.sh
@@ -4,16 +4,14 @@
 
 set -Eeo pipefail
 shopt -s inherit_errexit
-export LANG=C.UTF-8
-export LC_ALL=C.UTF-8
+# export LANG=C.UTF-8
+# export LC_ALL=C.UTF-8
 IFS=$'\n\t'
 
-readonly VERSION=0.1.1
+readonly VERSION=0.1.2
 export VERSION
 readonly ASSETS_DIR=assets
 export ASSETS_DIR
-readonly BUILD_DIR=themes/onimai_mahiro_yukata
-export BUILD_DIR
 readonly FONT_FILENAME="MapleMono-NF-CN-Regular.ttf"
 export FONT_FILENAME
 readonly FFMPEG_LOGLEVEL="repeat+level+time+datetime+trace"
@@ -23,7 +21,7 @@ echo_err() {
     echo "Error: $*" >&2
 }
 
-is_gt_zero() {
+is_positive() {
     local str
     str="$(printf '%s' "$1" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
     [ -z "$str" ] && \
@@ -64,68 +62,70 @@ Options:
          Output version information and exit.
   -h, --help
          Display this help and exit.
+  -S, --suffix=<suffix>
+         Append a suffix to the release.
+         Note: You may need to add an underscore (_) at the beginning of the
+         suffix.
+         Warning: To ensure maximum compatibility, it is suggested that the
+         suffix contains lowercase letters, numbers, and underscores only.
   -s, --scale=<scale>
          Specify the zoom scale of elements except the background.
          Character illustrations included in the background.
-         Note: If expected to show larger elements on high-resolution
+         Note: If expected to show larger and fewer elements on high-resolution
          displayers, the scale should be set to 1.5 and even bigger.
   -c, --color={ pink | blue }
          Specify the boot menu item color.
          If not specified, the color will be 'pink'.
          Pink is with female symbols, blue with male ones.
-  -l, --language={ en | zh-cn | zh-tr | ja }
+  -l, --language=<language>
          Sepcify the timeout prompt language.
-         If not specified, the language will be 'en'.
+         Available languages: 'en' (default), 'zh-CN', 'zh-TW', 'es', 'fr',
+         'de', 'ja', 'ko', 'ru', 'ar', 'pt', 'hi', 'it'.
 
 Examples:
-  $0 -l zh-cn
-  $0 -s 1.5 -c blue -l zh-tr
+  $0 -l zh-CN
+  $0 -s 1.5 -c blue -l zh-TW
   $0 --color=pink --language=ja
-  $0 --scale 0.75 --color blue --language en
+  $0 --scale 0.75 --color blue --language en --suffix=_tux
 EOF
 }
 
 main() {
     # ===== Argument Parsing =====
 
-    opt_short=Vhs:c:l:
-    opt_long=version,help,scale:,color:,language:
+    opt_short=Vhs:S:c:l:
+    opt_long=version,help,scale:,suffix:,color:,language:
 
     opt="$(getopt -o "$opt_short" -l "$opt_long" -n "$0" -- "$@")"
     eval set -- "$opt"
 
-    has_flag_V=0
-    has_flag_h=0
-    has_option_s=0
-    option_s_value=""
-    has_option_c=0
-    option_c_value=""
-    has_option_l=0
-    option_l_value=""
+    declare -A flags
+    declare -A opts
     
     while true; do
         case "$1" in
             -V|--version)
-                has_flag_V=1
+                flags[V]=
                 shift 1
                 ;;
             -h|--help)
-                has_flag_h=1
+                flags[h]=
                 shift 1
                 ;;
             -s|--scale)
-                has_option_s=1
-                option_s_value="$2"
+                opts[s]="$2"
+                shift 2
+                ;;
+            -S|--suffix)
+                opts[S]="$2"
                 shift 2
                 ;;
             -c|--color)
-                has_option_c=1
-                option_c_value="$2"
+                opts[c]="$2"
                 shift 2
                 ;;
             -l|--language)
-                has_option_l=1
-                option_l_value="$2"
+                opts[l]="$2"
                 shift 2
                 ;;
             --)
@@ -141,48 +141,62 @@ main() {
 
     # ===== Argument Check =====
 
-    (( has_flag_V )) && {
+    # script information
+
+    [[ -v flags[V] ]] && {
         echo $VERSION
         return 0
     }
-    (( has_flag_h )) && {
+    [[ -v flags[h] ]] && {
         usage
         return 0
     }
+
+    # operands
 
     [[ $# -eq 0 ]] || {
         echo_err "too many arguments"
         return 1
     }
 
-    (( has_option_s )) && {
-        is_gt_zero "$option_s_value" || {
-            echo_err "invalid scale '$option_s_value'"
+    # options
+
+    [[ -v opts[s] ]] && {
+        is_positive "${opts[s]}" || {
+            echo_err "invalid scale '${opts[s]}'"
             return 1
         }
     }
 
-    if (( has_option_c )); then
-        [[ "$option_c_value" =~ ^(pink|blue)$ ]] || {
-            echo_err "color '$option_c_value' not supported"
+    [[ -v opts[c] ]] && {
+        [[ "${opts[c]}" =~ ^(pink|blue)$ ]] || {
+            echo_err "color '${opts[c]}' not supported"
             return 1
         }
-    fi
-    if (( has_option_l )); then
-        [[ "$option_l_value" =~ ^(en|zh-cn|zh-tr|ja)$ ]] || {
-            echo_err "language '$option_l_value' not supported"
-            return 1
-        }
-    fi
+    }
 
-    if (( has_option_s )); then
-        scale=$option_s_value
-    else
-        scale=1
-    fi
+    [[ -v opts[l] ]] && {
+        [[ "${opts[l]}" =~ ^(en|zh-CN|zh-TW|es|fr|de|ja|ko|ru|ar|pt|hi|it)$ ]] || {
+            echo_err "language '${opts[l]}' not supported"
+            return 1
+        }
+    }
+
+    # ===== Variable Calculation =====
+
+    # non-export
+
+    build_dir="themes/onimai_mahiro_yukata${opts[S]}"
+
+    scale=1
+    [[ -v opts[s] ]] && \
+        scale=${opts[s]}
+
+    item_color=pink
+    [[ -v opts[c] ]] && \
+        item_color=${opts[c]}
+
     item_w_width=$(float_multiple_round 24 "$scale")
-
-    # ===== Theme Element Location & Size Calculation =====
 
     # fonts
 
@@ -217,9 +231,9 @@ main() {
     ITEM_HEIGHT=$(float_multiple_round 68 "$scale")
     ITEM_ICON_SPACE=$(float_multiple_round 18 "$scale")
     ITEM_SPACING=$(float_multiple_round 16 "$scale")
-    [[ $option_c_value = pink ]] && \
+    [[ ${opts[c]} = pink ]] && \
         ITEM_COLOR_HEX=#ee858c
-    [[ $option_c_value = blue ]] && \
+    [[ ${opts[c]} = blue ]] && \
         ITEM_COLOR_HEX=#45bbff
 
     # timeout prompt
@@ -233,12 +247,30 @@ main() {
         )"
     )
     TIMEOUT_TEXT="Selected OS will be booted in %d seconds"
-    [[ $option_l_value = zh-cn ]] && \
+    [[ ${opts[l]} = zh-CN ]] && \
         TIMEOUT_TEXT="所选操作系统将在 %d 秒后启动"
-    [[ $option_l_value = zh-tr ]] && \
-        TIMEOUT_TEXT="所選操作系統將在 %d 秒後啟動"
-    [[ $option_l_value = ja ]] && \
-        TIMEOUT_TEXT="選択したOSは %d 秒後に起動します"
+    [[ ${opts[l]} = zh-TW ]] && \
+        TIMEOUT_TEXT="所選作業系統將在 %d 秒後啟動"
+    [[ ${opts[l]} = es ]] && \
+        TIMEOUT_TEXT="El sistema operativo seleccionado se iniciará en %d segundos"
+    [[ ${opts[l]} = fr ]] && \
+        TIMEOUT_TEXT="Le système d'exploitation sélectionné sera démarré dans %d secondes"
+    [[ ${opts[l]} = de ]] && \
+        TIMEOUT_TEXT="Das ausgewählte Betriebssystem wird in %d Sekunden gestartet"
+    [[ ${opts[l]} = ja ]] && \
+        TIMEOUT_TEXT="選択されたOSは %d 秒後に起動します"
+    [[ ${opts[l]} = ko ]] && \
+        TIMEOUT_TEXT="선택한 운영 체제가 %d 초 후에 부팅됩니다"
+    [[ ${opts[l]} = ru ]] && \
+        TIMEOUT_TEXT="Выбранная ОС будет загружена через %d секунд."
+    [[ ${opts[l]} = ar ]] && \
+        TIMEOUT_TEXT="سيتم تشغيل نظام التشغيل المحدد خلال %d ثانية"
+    [[ ${opts[l]} = pt ]] && \
+        TIMEOUT_TEXT="O sistema operacional selecionado será inicializado em %d segundos"
+    [[ ${opts[l]} = hi ]] && \
+        TIMEOUT_TEXT="चयनित OS %d सेकंड में बूट हो जाएगा"
+    [[ ${opts[l]} = it ]] && \
+        TIMEOUT_TEXT="Il sistema operativo selezionato verrà avviato in %d secondi"
         
     # terminal
 
@@ -256,11 +288,11 @@ main() {
 
     # create necessary directories
 
-    mkdir -v -p $BUILD_DIR/icons
+    mkdir -v -p "$build_dir/icons"
 
     # copy background
 
-    cp -v $ASSETS_DIR/images/background.png $BUILD_DIR
+    cp -v $ASSETS_DIR/images/background.png "$build_dir"
 
     # create fonts
 
@@ -270,24 +302,24 @@ main() {
         grub-mkfont -v \
             --name="$FONT_NAME" \
             --size="$font_size" \
-            --output="$BUILD_DIR/${FONT_FILENAME%.*}-$font_size.pf2" \
+            --output="$build_dir/${FONT_FILENAME%.*}-$font_size.pf2" \
             $ASSETS_DIR/fonts/$FONT_FILENAME
     done
 
     # copy font license
 
-    cp -v $ASSETS_DIR/fonts/OFL.txt $BUILD_DIR
+    cp -v $ASSETS_DIR/fonts/OFL.txt "$build_dir"
 
     # copy terminal box elements
 
-    cp -v $ASSETS_DIR/images/terminal_box/* $BUILD_DIR
+    cp -v $ASSETS_DIR/images/terminal_box/* "$build_dir"
 
     # convert title
 
     ffmpeg -loglevel $FFMPEG_LOGLEVEL \
         -i $ASSETS_DIR/images/title.png \
         -vf "scale=$TITLE_WIDTH:-1" \
-        $BUILD_DIR/title.png
+        "$build_dir/title.png"
 
     # convert distro icons
 
@@ -295,33 +327,28 @@ main() {
         ffmpeg -loglevel $FFMPEG_LOGLEVEL \
             -i "$icon" \
             -vf "scale=$ICON_SIZE:$ICON_SIZE" \
-            "$BUILD_DIR/icons/$(basename "$icon")"
+            "$build_dir/icons/$(basename "$icon")"
     done
 
     # convert unselected item elements
-
-    # set item color
-    item_color=pink
-    (( has_option_c )) && \
-        item_color=$option_c_value
 
     # west
     ffmpeg -loglevel $FFMPEG_LOGLEVEL \
         -i "$ASSETS_DIR/images/item/$item_color/item_w_c.png" \
         -vf "scale=$item_w_width:$ITEM_HEIGHT" \
-        $BUILD_DIR/item_w.png
+        "$build_dir/item_w.png"
 
     # central
     ffmpeg -loglevel $FFMPEG_LOGLEVEL \
         -i "$ASSETS_DIR/images/item/$item_color/item_w_c.png" \
         -vf "scale=1:$ITEM_HEIGHT" \
-        $BUILD_DIR/item_c.png
+        "$build_dir/item_c.png"
 
     # east
     ffmpeg -loglevel $FFMPEG_LOGLEVEL \
         -i "$ASSETS_DIR/images/item/$item_color/item_e.png" \
         -vf "scale=-1:$ITEM_HEIGHT" \
-        $BUILD_DIR/item_e.png
+        "$build_dir/item_e.png"
 
     # convert selected item elements
 
@@ -329,19 +356,19 @@ main() {
     ffmpeg -loglevel $FFMPEG_LOGLEVEL \
         -i "$ASSETS_DIR/images/selected_item/$item_color/selected_item_w_c.png" \
         -vf "scale=$item_w_width:$ITEM_HEIGHT" \
-        $BUILD_DIR/selected_item_w.png
+        "$build_dir/selected_item_w.png"
     
     # central
     ffmpeg -loglevel $FFMPEG_LOGLEVEL \
         -i "$ASSETS_DIR/images/selected_item/$item_color/selected_item_w_c.png" \
         -vf "scale=1:$ITEM_HEIGHT" \
-        $BUILD_DIR/selected_item_c.png
+        "$build_dir/selected_item_c.png"
 
     # east
     ffmpeg -loglevel $FFMPEG_LOGLEVEL \
         -i "$ASSETS_DIR/images/selected_item/$item_color/selected_item_e.png" \
         -vf "scale=-1:$ITEM_HEIGHT" \
-        $BUILD_DIR/selected_item_e.png
+        "$build_dir/selected_item_e.png"
 
     # generate theme configuration
 
@@ -368,7 +395,7 @@ main() {
     export ITEM_SPACING
     export ITEM_COLOR_HEX
 
-    envsubst < $ASSETS_DIR/theme.txt.conf | tee $BUILD_DIR/theme.txt
+    envsubst < $ASSETS_DIR/theme.txt.conf | tee "$build_dir/theme.txt"
 }
 
 if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
